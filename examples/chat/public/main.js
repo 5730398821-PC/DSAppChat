3820dfc17f4a5b8176615d0d64cbc8eb7aca3c64
zFord Nutthawat <zfordn@gmail.com>
2017-04-21 07:09:34 +0700

    Add Notify.js

--- examples/chat/public/main.js
+++ examples/chat/public/main.js
@@ -1,10 +1,11 @@
-$(function() {
+$(function () {
   var FADE_TIME = 150; // ms
   var TYPING_TIMER_LENGTH = 400; // ms
   var COLORS = [
     '#e21400', '#91580f', '#f8a700', '#f78b00',
     '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
-    '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
+    '#3b88eb', '#3824aa', '#a700ff', '#d300e7',
+    '#1bb76a', '#a33bcd', '#8b72b1', '#dcba4d'
   ];
 
   // Initialize variables
@@ -17,7 +18,11 @@ $(function() {
   var $loginPage = $('.login.page'); // The login page
   var $chatPage = $('.chat.page'); // The chatroom page
 
+  var $sleepButton = $('.sleepButton');
+  var $sleepButton2 = $('.sleepButton2');
+  var $unreadButton = $('.unreadButton');
   var $leaveButton = $('.leaveButton');
+
 
   // Prompt for setting a username
   var username;
@@ -25,11 +30,13 @@ $(function() {
   var connected = false;
   var typing = false;
   var lastTypingTime;
-  var $currentInput;//= $usernameInput.focus();
+  var $currentInput; //= $usernameInput.focus();
 
   var socket = io();
 
-  function addParticipantsMessage (data) {
+  $sleepButton2.hide();
+
+  function addParticipantsMessage(data) {
     var message = '';
     if (data.numUsers === 1) {
       message += "there's 1 people in this room";
@@ -39,8 +46,52 @@ $(function() {
     log(message);
   }
 
+  function Notify(text, callback, close_callback, style) {
+
+    var time = '10000';
+    var $container = $('#notifications');
+    var icon = '<i class="fa fa-info-circle "></i>';
+
+    if (typeof style == 'undefined') style = 'warning'
+
+    var html = $('<div class="alert alert-' + style + '  hide">' + icon + " " + text + '</div>');
+
+    $('<a>', {
+      text: '×',
+      class: 'button close',
+      style: 'padding-left: 10px;',
+      href: '#',
+      click: function (e) {
+        e.preventDefault()
+        close_callback && close_callback()
+        remove_notice()
+      }
+    }).prependTo(html)
+
+    $container.prepend(html)
+    html.removeClass('hide').hide().fadeIn('slow')
+
+    function remove_notice() {
+      html.stop().fadeOut('slow').remove()
+    }
+
+    var timer = setInterval(remove_notice, time);
+
+    $(html).hover(function () {
+      clearInterval(timer);
+    }, function () {
+      timer = setInterval(remove_notice, time);
+    });
+
+    html.on('click', function () {
+      clearInterval(timer)
+      callback && callback()
+      remove_notice()
+    });
+  }
+
   // Sets the client's username
-  function setUsername () {
+  function setUsername() {
     username = cleanInput($usernameInput.val().trim());
     groupID = cleanInput($groupInput.val().trim());
     // If the username is valid
@@ -57,7 +108,7 @@ $(function() {
   }
 
   // Sends a chat message
-  function sendMessage () {
+  function sendMessage() {
     var message = $inputMessage.val();
 
     // Prevent markup from being injected into the message
@@ -81,13 +132,13 @@ $(function() {
   }
 
   // Log a message
-  function log (message, options) {
+  function log(message, options) {
     var $el = $('<li>').addClass('log').text(message);
     addMessageElement($el, options);
   }
 
   // Adds the visual chat message to the message list
-  function addChatMessage (data, options) {
+  function addChatMessage(data, options) {
     // Don't fade the message in if there is an 'X was typing'
     var $typingMessages = getTypingMessages(data);
     options = options || {};
@@ -101,8 +152,8 @@ $(function() {
       .css('color', getUsernameColor(data.username));
     var $messageBodyDiv = $('<span class="messageBody">')
       .text(data.message);
-     var $timestampBodyDiv = $('<span class="timestampBody">   ')
-       .text(data.timestamp);
+    var $timestampBodyDiv = $('<span class="timestampBody">   ')
+      .text(data.timestamp);
 
     var typingClass = data.typing ? 'typing' : '';
     var $messageDiv = $('<li class="message"/>')
@@ -114,14 +165,14 @@ $(function() {
   }
 
   // Adds the visual chat typing message
-  function addChatTyping (data) {
+  function addChatTyping(data) {
     data.typing = true;
     data.message = 'is typing';
     addChatMessage(data);
   }
 
   // Removes the visual chat typing message
-  function removeChatTyping (data) {
+  function removeChatTyping(data) {
     getTypingMessages(data).fadeOut(function () {
       $(this).remove();
     });
@@ -132,7 +183,7 @@ $(function() {
   // options.fade - If the element should fade-in (default = true)
   // options.prepend - If the element should prepend
   //   all other messages (default = false)
-  function addMessageElement (el, options) {
+  function addMessageElement(el, options) {
     var $el = $(el);
 
     // Setup default options
@@ -159,12 +210,12 @@ $(function() {
   }
 
   // Prevents input from having injected markup
-  function cleanInput (input) {
+  function cleanInput(input) {
     return $('<div/>').text(input).text();
   }
 
   // Updates the typing event
-  function updateTyping () {
+  function updateTyping() {
     if (connected) {
       if (!typing) {
         typing = true;
@@ -184,18 +235,18 @@ $(function() {
   }
 
   // Gets the 'X is typing' messages of a user
-  function getTypingMessages (data) {
+  function getTypingMessages(data) {
     return $('.typing.message').filter(function (i) {
       return $(this).data('username') === data.username;
     });
   }
 
   // Gets the color of a username through our hash function
-  function getUsernameColor (username) {
+  function getUsernameColor(username) {
     // Compute hash code
     var hash = 7;
     for (var i = 0; i < username.length; i++) {
-       hash = username.charCodeAt(i) + (hash << 5) - hash;
+      hash = username.charCodeAt(i) + (hash << 5) - hash;
     }
     // Calculate color
     var index = Math.abs(hash % COLORS.length);
@@ -203,27 +254,36 @@ $(function() {
   }
 
   //Button events
-    $leaveButton.click(function(){
-      //socket.emit('leave room');
-      var r = confirm("Are you sure you want to leave group ?");
-      if (r == true) {
-        window.location.reload();
-      }
-      //window.location.reload();
-      //alert("Leave Button was clicked.");
-      // $chatPage.fadeOut();
-      // $loginPage.show();
-      // $chatPage.off('click');
-      // $currentInput = $usernameInput.focus();
-    });
+  $leaveButton.click(function () {
+    //socket.emit('leave room');
+    var r = confirm("Are you sure you want to leave group ?");
+    if (r == true) {
+      window.location.reload();
+    }
+  });
+
+  $sleepButton.click(function () {
+    alertify.error('Notification Off');
+    $sleepButton2.show();
+    $sleepButton.hide();
+
+
+  });
+
+  $sleepButton2.click(function () {
+    //socket.emit('leave room');
+    alertify.success('Notification On');
+    $sleepButton2.hide();
+    $sleepButton.show();
+  });
 
   // Keyboard events
 
   $window.keydown(function (event) {
     // Auto-focus the current input when a key is typed
-   // if (!(event.ctrlKey || event.metaKey || event.altKey)) {
-   //   $currentInput.focus();
-   // }
+    // if (!(event.ctrlKey || event.metaKey || event.altKey)) {
+    //   $currentInput.focus();
+    // }
     // When the client hits ENTER on their keyboard
     if (event.which === 13) {
       if (username) {
@@ -236,7 +296,7 @@ $(function() {
     }
   });
 
-  $inputMessage.on('input', function() {
+  $inputMessage.on('input', function () {
     updateTyping();
   });
 
@@ -309,14 +369,13 @@ $(function() {
   });
 
 });
-	socket.on('updaterooms', function(rooms, current_room) {
-		$('#rooms').empty();
-		$.each(rooms, function(key, value) {
-			if(value == current_room){
-				$('#rooms').append('<div>' + value + '</div>');
-			}
-			else {
-				$('#rooms').append('<div><a href="#" onclick="switchRoom(\''+value+'\')">' + value + '</a></div>');
-			}
-		});
-	});
+socket.on('updaterooms', function (rooms, current_room) {
+  $('#rooms').empty();
+  $.each(rooms, function (key, value) {
+    if (value == current_room) {
+      $('#rooms').append('<div>' + value + '</div>');
+    } else {
+      $('#rooms').append('<div><a href="#" onclick="switchRoom(\'' + value + '\')">' + value + '</a></div>');
+    }
+  });
+});
